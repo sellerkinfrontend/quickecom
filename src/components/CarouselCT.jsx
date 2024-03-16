9bf4126c2512fd0c05982b800b3b4ae5b793a727
Aditya J <aditya19137@iiitd.ac.in>
2024-03-16 16:56:18 +0530

    stars updated

--- src/components/CarouselCT.jsx
+++ src/components/CarouselCT.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import star from "../assets/stars/star.png";
+import star from "../assets/stars/Capture.JPG";
 const CarouselCT = () => {
   const settings = {
     dots: true,
